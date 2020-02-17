b2d31600b5da349ae934add0d00dd3ad8fe2b819
Yasya El Hakim <yasyaelhakim@gmail.com>
2020-02-17 12:42:03 +0700

    Update script.js

--- AUTOPREFIXES BLOGGER POST/script/script.js
+++ AUTOPREFIXES BLOGGER POST/script/script.js
@@ -8,14 +8,15 @@ function code_clear() {
 
 function code_convert(b) {
     var c = 1,
-        f = 0,
-        e = "";
+    f = 0,
+    e = "";
     b = document.getElementById("tarea");
     var a = b.value;
     a = a.replace(/<h([\d]).*?>[\n](.*?)<\/h([\d]).*?>/gi, function(a, d, b, g) {
         return d != g ? a : (d > c ? e += Array(d - c + 1).join("<ol class='point" + c + "'>") : d < c && (e += Array(c - d + 1).join("</ol>")), f += 1, e += '<li><a href="#point' + f + '" title="' + b + '">' + b + "</a></li>", c = parseInt(d), "<h" + d + " id='point" + f + "'>" + b + "</h" + g + ">")
     });
     c && (e += Array(c - 1).join("</ol>"));
+
     a = a.replace('<div id="elcTOC">\n</div>', '<div id="elcTOC">' + e + "</ol></div>");
     a = a.replace(RegExp('<div class="separator( [^"]*?)?"(>|\\s[^<>]*?>)([\\s\\S]*?)<\\/div>', "g"), '<figure class="separator$1"$2$3\n</figure>');
     a = a.replace(RegExp('<blockquote class="tr_bq( [^"]*?)?"(>|\\s[^<>]*?>)([\\s\\S]*?)<\\/blockquote>', "g"), '<blockquote class="tr_bq$1"$2$3\n</blockquote>');
