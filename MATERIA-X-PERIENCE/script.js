e4242bb6f0bd08044a8d12541ec754a5b27474e4
Yasya El Hakim <yasyaelhakim@gmail.com>
2020-10-31 22:31:53 +0700

    update

--- MATERIA-X-PERIENCE/script.js
+++ MATERIA-X-PERIENCE/script.js
@@ -1,34 +1,37 @@
 var enabled = true;
 
 var req = new XMLHttpRequest();
-req.open('GET', chrome.runtime.getURL('elcreative-materiaxperience.css'));
-req.onload = function() {
+req.open("GET", chrome.runtime.getURL("elcreative-materiaxperience.css"));
+req.onload = function () {
   if (req.readyState === 4)
     if (req.status === 200) {
-      var oNew = document.createElement('style');
-      oNew.rel = 'stylesheet';
-      oNew.type = 'text/css';
-      oNew.media = 'screen';
-      oNew.id = 'elcreative-materiaxperience';
+      var oNew = document.createElement("style");
+      oNew.rel = "stylesheet";
+      oNew.type = "text/css";
+      oNew.media = "screen";
+      oNew.id = "elcreative-materiaxperience";
       oNew.textContent = req.responseText;
       document.documentElement.appendChild(oNew);
     }
+};
+req.send(null);
+
+var mutation = new MutationObserver(() => {
+  if (enabled) {
+    document.documentElement.appendChild(oNew);
   }
-  req.send(null);
+  mutation.disconnect();
+});
 
-  var mutation = new MutationObserver(() => {
-    if (enabled) {
-      document.documentElement.appendChild(oNew);
-    }
-    mutation.disconnect();
-  });
-
-  chrome.storage.local.get({
-    enabled: true
-  }, prefs => {
+chrome.storage.local.get(
+  {
+    enabled: true,
+  },
+  (prefs) => {
     enabled = prefs.enabled;
     if (enabled) {
       document.documentElement.appendChild(oNew);
     }
     mutation.observe(document, { childList: true, subtree: true });
-  });
+  }
+);
--- MATERIA-X-PERIENCE/src/popup.js
+++ MATERIA-X-PERIENCE/src/popup.js
@@ -1,23 +1,24 @@
-let themeSwitch = document.getElementById('theme-switch');
+let themeSwitch = document.getElementById("theme-switch");
 
-themeSwitch.onchange = function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function() {
-        if(themeSwitch.checked){
-            chrome.tabs.executeScript(
-                {code: 'document.querySelector("#elcreative-materiaxperience").textContent = req.responseText;'
-            });
-            localStorage.setItem('toggle-theme', 'on');
-        }
-        else{
-            chrome.tabs.executeScript(
-                {code: 'document.querySelector("#elcreative-materiaxperience").textContent = "";'
-            });
-            localStorage.setItem('toggle-theme', 'off');
-        }
-    });
+themeSwitch.onchange = function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function () {
+    if (themeSwitch.checked) {
+      chrome.tabs.executeScript({
+        code:
+          'document.querySelector("#elcreative-materiaxperience").textContent = req.responseText;',
+      });
+      localStorage.setItem("toggle-theme", "on");
+    } else {
+      chrome.tabs.executeScript({
+        code:
+          'document.querySelector("#elcreative-materiaxperience").textContent = "";',
+      });
+      localStorage.setItem("toggle-theme", "off");
+    }
+  });
 };
 
-window.onload = function(){
-	themeSwitch.checked = localStorage.getItem('toggle-theme')!='off';
-    themeSwitch.onchange();
+window.onload = function () {
+  themeSwitch.checked = localStorage.getItem("toggle-theme") != "off";
+  themeSwitch.onchange();
 };
